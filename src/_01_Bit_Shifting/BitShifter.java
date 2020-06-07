8c602f14a13a6e56c523b57edf004f5d74686964
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2020-06-07 15:56:39 -0700

    bitshifter, bitprinter, base64decoder(IP),
    ALMOST DONE WITH LEVEL

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -3,39 +3,33 @@ package _01_Bit_Shifting;
 public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
-		int num = 4;
-		int numShifted = num << 1;
+		String binaryStr="";
+		        	int num = 4;
 
+        do {
+    		int numShifted = num >> 1;
 		// 2. Print the value of numShifted, and convert that value to binary.
-		String binary = "";
-
-		System.out.println(numShifted);
-		while (numShifted != 0) {
-
-			int numShiftedTwo = numShifted >>> 1;
-			if (numShifted % 2 != 0) {
-
-				binary = '1' + binary;
-
-			}
-
-			else {
-				binary = '0' + binary;
-			}
-
-			numShifted = numShiftedTwo;
-		}
-
-		System.out.println(binary);
+            System.out.println(numShifted);
+            
+            if( num % 2 != 0 ){
+                binaryStr = '1' + binaryStr;
+            } else {
+                binaryStr = '0' + binaryStr;
+            }
+            
+            num = numShifted;
+            
+        // 3. Repeat until number is 0
+        } while( num != 0 );
+        
+        System.out.println(binaryStr); 
+    }
 		// 3. Compare the two binary values. Can you figure out what the << operator is
 		// for?
 
 		// 4. Try shifting num 3 places.
 
-		num = num << 3;
-		System.out.println(num);
-
-
 		// FYI: Binary values can be shifted to the right as well using the >> operator.
+
 	}
-}
+
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -8,23 +8,93 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
-		
-	}
+		String binaryStr="";
+		String sNum = Byte.toString(b);
+		int num = Integer.parseInt(sNum);
+	       do {
+	   		int numShifted =num >> 1;
+	           
+	           if( num % 2 != 0 ){
+	               binaryStr = '1' + binaryStr;
+	           } else {
+	               binaryStr = '0' + binaryStr;
+	           }
+	           
+	           num = numShifted;
+	           
+	       } while( num != 0 );
+	       
+	       System.out.println(binaryStr); 	}
 	
 	public void printShortBinary(short s) {
+		String binaryStr="";
+		String sNum = Short.toString(s);
+		int num = Integer.parseInt(sNum);
+	       do {
+	   		int numShifted =num >> 1;
+	           
+	           if( num % 2 != 0 ){
+	               binaryStr = '1' + binaryStr;
+	           } else {
+	               binaryStr = '0' + binaryStr;
+	           }
+	           
+	           num = numShifted;
+	           
+	       } while( num != 0 );
+	       
+	       System.out.println(binaryStr); 	
+	   }
 		
-	}
+	
+	
 	
 	public void printIntBinary(int i) {
+		String binaryStr="";
+	       do {
+	   		int numShifted =i >> 1;
+	           
+	           if( i % 2 != 0 ){
+	               binaryStr = '1' + binaryStr;
+	           } else {
+	               binaryStr = '0' + binaryStr;
+	           }
+	           
+	           i = numShifted;
+	           
+	       } while( i != 0 );
+	       
+	       System.out.println(binaryStr); 
+	   }
 		
-	}
+	
 	
 	public void printLongBinary(long l) {
-		
+		String binaryStr="";
+		String sNum = Long.toString(l);
+		int num = Integer.parseInt(sNum);
+	       do {
+	   		int numShifted =num >> 1;
+	           
+	           if( num % 2 != 0 ){
+	               binaryStr = '1' + binaryStr;
+	           } else {
+	               binaryStr = '0' + binaryStr;
+	           }
+	           
+	           num = numShifted;
+	           
+	       } while( num != 0 );
+	       
+	       System.out.println(binaryStr); 	
+
 	}
 	
 	public static void main(String[] args) {
 		BinaryPrinter bp = new BinaryPrinter();
-		
-	}
+		bp.printByteBinary((byte) 10);
+		bp.printShortBinary((short) 10);
+		bp.printIntBinary(10);
+		bp.printLongBinary((long) 10);
+		}
 }
